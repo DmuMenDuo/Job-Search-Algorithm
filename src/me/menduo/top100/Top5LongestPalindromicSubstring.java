cc82f2127bc8a98dbd0d49a89f2c970448258324
menduo <menduo@dlmu.edu.cn>
2017-12-06 19:04:13 +0800

    add top5 solution.

---
+++ src/me/menduo/top100/Top5LongestPalindromicSubstring.java
@@ -0,0 +1,44 @@
+package me.menduo.top100;
+
+public class Top5LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        int start=0,end=0;
+        for(int i=0;i<s.length();i++){
+            int len1 = findPalindrome(s,i,i);
+            int len2 = findPalindrome(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if(len > end-start){
+                start = i-(len-1)/2;
+                end = i + len/2;
+            }
+        }
+        return s.substring(start,end+1);
+
+    }
+    int findPalindrome(String s,int l,int r){
+        while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+            //这里l多减了,r多加了，所以最后结果要减2 r-l+1-2 = r-l-1;
+            l--;
+            r++;
+        }
+        return r-l-1;
+    }
+    public static void main(String[] args) {
+        Top5LongestPalindromicSubstring top5LongestPalindromicSubstring = new Top5LongestPalindromicSubstring();
+//        Example:
+//
+//        Input: "babad"
+//
+//        Output: "bab"
+//
+//        Note: "aba" is also a valid answer.
+        System.out.println(top5LongestPalindromicSubstring.longestPalindrome("babad"));
+//        Example:
+//
+//        Input: "cbbd"
+//
+//        Output: "bb"
+        System.out.println(top5LongestPalindromicSubstring.longestPalindrome("cbbd"));
+    }
+}
