3a98f132bd25ccb3ed13dd13f615e8ef1246c3f9
menduo <menduo@dlmu.edu.cn>
2018-04-19 23:42:00 +0800

    4.19 求一个字符串最大字典序子串

---
+++ src/me/menduo/top100/NO819MostCommonWord.java
@@ -0,0 +1,40 @@
+package me.menduo.top100;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class NO819MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        //paragraph = paragraph.replaceAll("\\pP","");
+        paragraph = paragraph.toLowerCase();
+        HashSet<String> set = new HashSet<>();
+        for(String x:banned) {
+            set.add(x);
+        }
+        int max=0;
+        String res=null;
+        String[] md = paragraph.split(" ");
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String x:md){
+            if(!Character.isLetter(x.charAt(x.length()-1)))
+                x=x.substring(0,x.length()-1);
+            if(map.containsKey(x)){
+                map.put(x,map.get(x)+1);
+
+            }else {
+                map.put(x,1);
+            }
+            if(!set.contains(x)&&map.get(x)>max) {
+                max=map.get(x);
+                res = x;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        NO819MostCommonWord no819MostCommonWord = new NO819MostCommonWord();
+        System.out.println(no819MostCommonWord.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit."
+                ,new String[]{"hit"}));
+    }
+}
