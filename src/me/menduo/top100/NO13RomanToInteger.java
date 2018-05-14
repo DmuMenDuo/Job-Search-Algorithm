2a7a3489be4d8b0101e48a8fff012dad52342840
menduo <menduo@dlmu.edu.cn>
2018-05-14 22:45:58 +0800

    5.14 数字转换 矩阵翻转

---
+++ src/me/menduo/top100/NO13RomanToInteger.java
@@ -0,0 +1,50 @@
+package me.menduo.top100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NO13RomanToInteger {
+    private void init(Map map) {
+        map.put("M", 1000);
+        map.put("CM", 900);
+        map.put("D", 500);
+        map.put("CD", 400);
+        map.put("C", 100);
+        map.put("XC", 90);
+        map.put("L", 50);
+        map.put("XL", 40);
+        map.put("X", 10);
+        map.put("IX", 9);
+        map.put("V", 5);
+        map.put("IV", 4);
+        map.put("I", 1);
+    }
+
+    public int romanToInt(String s) {
+        if (s == null || "".equals(s)) return 0;
+        Map<String, Integer> map = new HashMap<>();
+        init(map);
+        char[] c = s.toCharArray();
+        int res = 0;
+        int i = 0;
+        while (i < c.length) {
+            if (i + 1 < c.length && map.containsKey("" + c[i] + c[i + 1])) {
+                res += map.get("" + c[i] + c[i + 1]);
+                i += 2;
+            } else {
+                res += map.get("" + c[i]);
+                i++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        NO13RomanToInteger no13RomanToInteger = new NO13RomanToInteger();
+        System.out.println(no13RomanToInteger.romanToInt("III"));
+        System.out.println(no13RomanToInteger.romanToInt("IV"));
+        System.out.println(no13RomanToInteger.romanToInt("MCMXCIV"));
+        System.out.println(no13RomanToInteger.romanToInt("LVIII"));
+
+    }
+}
---
+++ src/me/menduo/top100/NO832FlippingAnImage.java
@@ -0,0 +1,36 @@
+package me.menduo.top100;
+
+public class NO832FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        if (A == null) return null;
+        for (int i = 0; i < A.length; i++) {
+            reverse(A[i]);
+            invert(A[i]);
+        }
+        return A;
+    }
+
+    private void reverse(int[] A) {
+        int head = 0, tail = A.length - 1;
+        int tmp;
+        while (head < tail) {
+            tmp = A[head];
+            A[head] = A[tail];
+            A[tail] = tmp;
+            head++;
+            tail--;
+        }
+    }
+
+    private void invert(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) A[i] = 1;
+            else A[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        NO832FlippingAnImage no832FlippingAnImage = new NO832FlippingAnImage();
+        System.out.println(no832FlippingAnImage.flipAndInvertImage(new int[][]{{1, 1, 0}, {1, 0, 1}, {0, 0, 0}}));
+    }
+}
