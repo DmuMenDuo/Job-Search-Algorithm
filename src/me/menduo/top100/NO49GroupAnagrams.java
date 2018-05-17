4384d381ea8e9e49cbdb9ebe836de00d48e24fcf
menduo <menduo@dlmu.edu.cn>
2018-05-17 13:37:45 +0800

    5.16 NO49 groupAnagrams

---
+++ src/me/menduo/top100/NO49GroupAnagrams.java
@@ -0,0 +1,26 @@
+package me.menduo.top100;
+
+import java.util.*;
+
+public class NO49GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return null;
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] cs = strs[i].toCharArray();
+            Arrays.sort(cs);
+            String key = String.valueOf(cs);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        NO49GroupAnagrams no49GroupAnagrams = new NO49GroupAnagrams();
+        no49GroupAnagrams.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
+}
