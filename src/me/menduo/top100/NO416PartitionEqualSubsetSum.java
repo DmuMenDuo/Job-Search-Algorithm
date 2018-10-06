3d1af716fe4781ca052b24cd6249203490bf45cc
menduo <demon9607@163.com>
2018-10-07 00:13:26 +0800

    方法就是 能否构成sum的一半 dfs方法

--- src/me/menduo/top100/NO416PartitionEqualSubsetSum.java
+++ src/me/menduo/top100/NO416PartitionEqualSubsetSum.java
@@ -9,7 +9,33 @@ import java.util.Arrays;
  * @create: 2018-10-06 22:54
  **/
 public class NO416PartitionEqualSubsetSum {
+    /**
+     * dfs方法更快
+     * @param nums
+     * @return
+     */
     public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            sum+=nums[i];
+        }
+
+        if((sum & 1) == 1) return false;
+        return helper(nums,nums.length-1,sum/2);
+    }
+
+    public boolean helper(int[] nums,int i,int sum) {
+        if(sum==0) return true;
+        else if(i<0 || sum<0||sum < nums[i]) return false;
+
+        else return helper(nums,i-1,sum-nums[i]) || helper(nums,i-1,sum);
+    }
+    /**
+     *  dp too slow
+     * @param nums
+     * @return
+     */
+    public boolean canPartition2(int[] nums) {
         if(nums == null || nums.length==0) return false;
         int sum = 0;
         for (int i = 0; i < nums.length ; i++) {
