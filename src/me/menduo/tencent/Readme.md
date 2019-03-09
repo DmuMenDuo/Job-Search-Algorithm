164c0584279f0a68e0d5fd0e40869ec3522970bc
menduo <demon9607@163.com>
2019-03-09 23:48:16 +0800

    腾讯提前批笔试-终于看懂系列，我好垃圾啊啊啊啊啊啊

---
+++ src/me/menduo/tencent/Readme.md
@@ -0,0 +1,3 @@
+
+dp[i][j] 表示前i张牌得j分的情况，递推公式 dp[i][j] =2*dp[i-1][j]+dp[i-1][j-1]
+![第三题](http://pic.menduo.xyz/20190309232206.png)
--- src/me/menduo/tencent/Three.java
+++ src/me/menduo/tencent/Three.java
@@ -1,7 +1,5 @@
 package me.menduo.tencent;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,31 +7,32 @@ import java.util.Scanner;
  * @mail: menduo96@gmail.com
  * @author: menduo
  * @create: 2019-03-09 20:30
+ *
  **/
 public class Three {
-    static int mod = 1000000007;
-    static int[][] cap = new int[2001][2001];
     public static void main(String[] args) {
-        cap[1][1] = 1;
-        for (int i = 2; i < 2001; i++) {
-            cap[i][1] = 1;
-            for (int j = 1; j <= i; j++) {
-                cap[i][j] = (cap[i - 1][j - 1]%mod + cap[i - 1][j]%mod)%mod;
+        Scanner sc =new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int s = sc.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < n ; i++) {
+                array[i] = sc.nextInt();
             }
-        }
+            int[][] dp = new int[2001][2001];
+            dp[0][0] = 1;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j <= s; j++) {
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int s = sc.nextInt();
-        List<Integer> list=new ArrayList();
-        int ans = cap[n+1][s+1];
-        for(int i = 0; i < n; i++){
-            list.add(sc.nextInt());
+                    dp[i][j] += 2 * dp[i - 1][j];
+                    dp[i][j] %= 1000000007;
+                    if (j > 0)
+                        dp[i][j] += dp[i - 1][j - 1];
+                    dp[i][j] %= 1000000007;
+                }
+            }
+            System.out.println(dp[n][s]);
         }
-        for(int i = 0;i < (n-s) ;i++){
-            ans = ans%mod * 2 %mod;
-        }
-        System.out.println(ans);
     }
 }
 
