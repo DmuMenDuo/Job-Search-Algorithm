7c45a79674d32922356fe411f9fe0ac041633ab1
menduo <demon9607@163.com>
2019-05-13 09:45:08 +0800

    美团第二题。树形dp。 有两种状态。 每种状态分黑白节点考虑。 然后dfs到叶子结点，逐步向上推。最后要求的就是dp[0][1]。中间涉及到一个知识点就是如何不用除法 解决除了某一个数的其他数的乘积。 这里用了前缀和后缀乘积记忆的方法。。也可以用逆元来做

--- src/me/menduo/meituan/Two.java
+++ src/me/menduo/meituan/Two.java
@@ -1,6 +1,6 @@
 package me.menduo.meituan;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @program: Job-Search-Algorithm
@@ -9,20 +9,74 @@ import java.util.Scanner;
  * @create: 2019-04-23 20:43
  **/
 public class Two {
+    static int MOD = 1000000007;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             int n = scanner.nextInt();
             int[] array = new int[n];
             int[] color = new int[n];
+            Map<Integer, List<Integer>> map = new HashMap<>();
             for (int i = 1; i < n ; i++) {
                 array[i] = scanner.nextInt();
+                if(map.containsKey(array[i])){
+                    map.get(array[i]).add(i);
+                }else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    map.put(array[i],list);
+                }
             }
             for (int i = 0; i < n ; i++) {
                 color[i] = scanner.nextInt();
             }
 
-            System.out.println(2);
+            long[][] dp = new long[n][2]; //dp状态转化
+            dfs(map, color, dp, 0);
+            System.out.println(dp[0][1]);
         }
+    }
+
+    private static void dfs(Map<Integer, List<Integer>> map, int[] color, long[][] dp, int n) {
+        //dfs遍历所有的节点
+        if(map.get(n) == null) {
+            if (color[n] == 0) dp[n][1] = 1;
+            else dp[n][0] = 1;
+            return;
+        }
+        for(int i:map.get(n)) dfs(map,color,dp, i);
+        //如果当前节点是白色的
+        if(color[n] == 0) {
+            dp[n][1] = 1;
+            for (int i = 0; i < map.get(n).size() ; i++) {
+                dp[n][1] = dp[n][1] * (dp[map.get(n).get(i)][0] + dp[map.get(n).get(i)][1]) % MOD;
+            }
+        }else {
+            int k = map.get(n).size();
+            int[] q = new int[k+2];
+            long[] l = new long[k+2];
+            long[] r = new long[k+2];
+            for (int i = 0; i < k; i++) {
+                q[i+1] = map.get(n).get(i);
+            }
+
+            l[0] = r[k+1] = 1;
+            for (int i = 1; i <= k; i++) {
+                l[i] = l[i-1] * (dp[q[i]][0] + dp[q[i]][1]) % MOD;
+            }
+
+            for (int i = k; i >= 1; i--) {
+                r[i] = r[i+1] * (dp[q[i]][0] + dp[q[i]][1]) % MOD;
+            }
+            dp[n][0] = 1;
+            for (int i = 1; i <= k ; i++) {
+                dp[n][0] = dp[n][0] * (dp[q[i]][0] + dp[q[i]][1]) % MOD;
+            }
+
+            for (int i = 1; i <= k ; i++) {
+                dp[n][1] = (dp[n][1] + l[i-1] % MOD * r[i+1] % MOD * dp[q[i]][1]) % MOD;
+            }
+        }
+
     }
 }
